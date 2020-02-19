54979c49dc06485684c88503a9c45062b0358477
shuai <shuaixie@zju.edu.cn>
2020-02-19 15:23:11 +0800

    F utils

---
+++ utils/F_utils.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+from datasets.transforms import get_transform
+from torchvision.transforms import functional as F
+import torchvision.transforms as transforms
+import os
+
+"""
+https://pytorch.org/docs/stable/torchvision/transforms.html#functional-transforms
+"""
+
+
+def show_img(img):
+    plt.imshow(img)
+    plt.show()
+
+
+def test_hflip(img):
+    show_img(img)
+    img = F.hflip(img)  # 左右翻转
+    show_img(img)
+
+
+def test_transform():
+    trans = get_transform(train=True)
+    tensor_to_pil = transforms.ToPILImage()
+
+    demo_dir = '../data/demo'
+    for img in os.listdir(demo_dir):
+        img = Image.open(os.path.join(demo_dir, img))
+        show_img(img)
+        img, _ = trans(img, target=None)
+        img = tensor_to_pil(img)
+        show_img(img)
+
+
+if __name__ == '__main__':
+    test_transform()
+    pass
